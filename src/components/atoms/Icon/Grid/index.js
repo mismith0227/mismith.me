96ac926fa676f110268d75c2d37f8d114ec1d2da
mismith0227 <doraepon2216@gmail.com>
2019-12-11 11:23:48 +0900

    #13 grid list表示 close

---
+++ src/components/atoms/Icon/Grid/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Icon from '..'
+
+const Grid = props => {
+  return (
+    <Icon viewBox="0 0 24 24" {...props}>
+      <path d="M16,5V11H21V5M10,11H15V5H10M16,18H21V12H16M10,18H15V12H10M4,18H9V12H4M4,11H9V5H4V11Z" />
+    </Icon>
+  )
+}
+
+export default Grid
--- src/components/organisms/WorkContent/index.js
+++ src/components/organisms/WorkContent/index.js
@@ -1,20 +1,39 @@
 import * as React from 'react'
-import get from 'lodash/get'
 import ArticlePreview from '~/components/organisms/ArticlePreview'
+import GridIcon from '~/components/atoms/Icon/Grid'
+import ListIcon from '~/components/atoms/Icon/List'
 
-import { Wrap, Title, LeadText, List, Item } from './styles'
+import {
+  Wrap,
+  Title,
+  LeadText,
+  List,
+  Item,
+  DisplayList,
+  DisplayItem,
+} from './styles'
 
 const WorkContent = ({ posts }) => {
+  const [display, setdisplay] = React.useState('grid')
+  console.log(display)
   return (
     <Wrap>
       <Title>Works</Title>
       <LeadText>制作した一部です。非公開のものもあります。</LeadText>
+      <DisplayList>
+        <DisplayItem onClick={() => setdisplay('grid')}>
+          <GridIcon />
+        </DisplayItem>
+        <DisplayItem onClick={() => setdisplay('list')}>
+          <ListIcon />
+        </DisplayItem>
+      </DisplayList>
       {posts && (
         <List>
           {posts.map(({ node }) => {
             return (
-              <Item key={node.slug}>
-                <ArticlePreview article={node} />
+              <Item key={node.slug} display={display}>
+                <ArticlePreview article={node} display={display} />
               </Item>
             )
           })}
