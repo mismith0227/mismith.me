5cc6c30059ae84d68919423447bd75e885b1bcd8
mismith0227 <doraepon2216@gmail.com>
2019-12-19 22:56:27 +0900

    layoutをfunctional componentへ

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,52 +1,26 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import * as React from 'react'
 import base from './base.css'
 import Cursor from '~/components/atoms/Cursor'
 import Navigation from '~/components/molecules/Navigation'
 
 import { Wrap, StyledMenuButton } from './styles'
 
-class Layout extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isSidebarOpen: false,
-      clientX: 0,
-      clientY: 0,
-    }
-  }
+const Layout = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState('')
 
-  handleSidebarCtrl() {
-    this.setState({ isSidebarOpen: !this.state.isSidebarOpen })
-  }
+  return (
+    <Wrap>
+      <Cursor />
+      <Navigation isOpen={isSidebarOpen} />
 
-  // handleMousemove(e) {
-  //   this.setState({ clientX: e.clientX, clientY: e.clientY })
-  // }
+      <StyledMenuButton
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        isOpen={isSidebarOpen}
+      />
 
-  render() {
-    const { children } = this.props
-
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
-
-    return (
-      <Wrap>
-        <Cursor />
-        <Navigation isOpen={this.state.isSidebarOpen} />
-
-        {this.state.isSidebarOpen}
-
-        <StyledMenuButton
-          onClick={this.handleSidebarCtrl.bind(this)}
-          isOpen={this.state.isSidebarOpen}
-        />
-
-        {children}
-      </Wrap>
-    )
-  }
+      {children}
+    </Wrap>
+  )
 }
 
 export default Layout
