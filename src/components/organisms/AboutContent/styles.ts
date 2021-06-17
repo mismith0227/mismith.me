fc3adb392bfeb6f2751aed6227c093b3af6df18d
mismith0227 <doraepon2216@gmail.com>
2021-06-17 14:29:41 +0900

    Skillまでのスタイリング

--- src/components/organisms/AboutContent/styles.ts
+++ src/components/organisms/AboutContent/styles.ts
@@ -1,16 +1,26 @@
 import styled from '@emotion/styled'
 
-export const Container = styled.main`
+export const Container = styled.div`
   display: block;
+  max-width: 1024px;
+  margin: 180px auto 0;
 `
 
 export const Title = styled.h2`
+  margin: 0;
   font-size: 64px;
 `
 
-export const Section = styled.section``
+export const Section = styled.section`
+  margin: 180px 0 0;
+`
+
+export const SectionInner = styled.div`
+  margin: 40px 0 0;
+`
 
 export const SectionTitle = styled.h3`
+  margin: 0;
   font-size: 42px;
 `
 
@@ -19,26 +29,20 @@ export const Text = styled.p``
 export const Tags = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  margin: 0;
+  margin: 24px 0 0;
   padding: 0;
   list-style: none;
 `
 
 export const TagItem = styled.li`
-  margin: 0 8px 0 0;
-
-  &::after {
-    content: '/';
-    margin: 0 0 0 8px;
-  }
+  margin: 8px 8px 0 0;
+  padding: 4px 8px;
+  background-color: #000;
+  color: #fff;
+  font-size: 14px;
 
   &:last-of-type {
-    margin: 0;
-
-    &::after {
-      display: none;
-      margin: 0;
-    }
+    margin-right: 0;
   }
 `
 
@@ -73,9 +77,20 @@ export const ActivityTerm = styled.dt``
 export const ActivityDescription = styled.dd``
 
 export const SnsList = styled.ul`
+  display: flex;
   margin: 0;
   padding: 0;
   list-style: none;
 `
 
-export const SnsItem = styled.li``
+export const SnsItem = styled.li`
+  margin: 32px 16px 0 0;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`
+
+export const ExternalLink = styled.a`
+  color: #000;
+`
