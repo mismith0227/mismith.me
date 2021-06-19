8d50c33e4265cc08cf350c037e32d30ddfb44736
mismith0227 <doraepon2216@gmail.com>
2021-06-20 07:46:16 +0900

    main animation

---
+++ src/components/organisms/HomeContent/index.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Tetrahedron } from '@react-three/drei'
+import { Container, Title } from './styles'
+
+type Props = {}
+
+export const HomeContent = ({}: Props) => {
+  const Circle = () => {
+    const ref = React.useRef<THREE.Mesh>(null!)
+    const [rotation, setRotation] = React.useState<number>(
+      Math.random() * Math.PI * 2
+    )
+    const [distance, setDistance] = React.useState<number>(
+      2.5 + Math.random() / 5
+    )
+    const [speed, setSpeed] = React.useState<number>(Math.random() / 200)
+
+    useFrame(() => {
+      ref.current.position.y = Math.sin(rotation) * distance
+      ref.current.position.x = Math.cos(rotation) * distance
+
+      ref.current.rotation.z += Math.random() * 0.1
+      ref.current.rotation.x += Math.random() * 0.05
+
+      setRotation(rotation - speed)
+    })
+    // Set receiveShadow on any mesh that should be in shadow,
+    // and castShadow on any mesh that should create a shadow.
+    return (
+      <Tetrahedron
+        castShadow
+        receiveShadow
+        ref={ref}
+        position={[0, 0, 0]}
+        args={[0.2, 0]}
+      >
+        <meshStandardMaterial attach="material" color="#888" />
+      </Tetrahedron>
+    )
+  }
+
+  return (
+    <Container>
+      <Canvas>
+        <directionalLight
+          intensity={0.5}
+          castShadow
+          shadow-mapSize-height={512}
+          shadow-mapSize-width={512}
+        />
+        {[...Array(24)].map((item, index) => (
+          <Circle key={index} />
+        ))}
+      </Canvas>
+
+      <Title>Web developer based in Osaka.</Title>
+    </Container>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { Canvas, useFrame } from '@react-three/fiber'
 import { Layout } from '../components/organisms/Layout'
 import Seo from '../components/seo'
-import { Description } from '../components/organisms/WorkContent/styles'
+import { HomeContent } from '../components/organisms/HomeContent'
 
 const IndexPage = () => {
   const meta = {
@@ -11,41 +9,11 @@ const IndexPage = () => {
     description: 'トップページ',
     path: 'home',
   }
-  const Box = (props: JSX.IntrinsicElements['mesh']) => {
-    const mesh = React.useRef<THREE.Mesh>(null!)
-    const [hovered, setHover] = React.useState(false)
-    const [active, setActive] = React.useState(false)
-    useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
-    return (
-      <mesh
-        {...props}
-        ref={mesh}
-        scale={active ? 1.5 : 1}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
-      >
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    )
-  }
 
   return (
     <Layout path={meta.path}>
       <Seo title={meta.title} description={meta.description} />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
-      <p>
-        <Link to="/works/">Works</Link>
-      </p>
+      <HomeContent />
     </Layout>
   )
 }
