942b25ad6350e6eb3c3cf85a88c77f4e801fef86
mismith0227 <mail@mismith.me>
2023-01-07 18:07:03 +0900

    モーダル追加

--- components/pages/PhotoContent/index.tsx
+++ components/pages/PhotoContent/index.tsx
@@ -1,7 +1,48 @@
-import { Wrap, StyledMasonry, StyledImage } from './styles'
+import { Modal } from '@mui/material'
+import { useState } from 'react'
+import {
+  Wrap,
+  StyledMasonry,
+  StyledImage,
+  ImageModal,
+  ModalImage,
+  CloseModal,
+  ScreenReaderText,
+} from './styles'
 import { Props } from './types'
 
+export type modalState = {
+  readonly isOpen: boolean
+  readonly src: string
+  readonly width: number
+  readonly height: number
+}
+
 export const PhotoContent = ({ data }: Props) => {
+  const [modalState, setModalState] = useState<modalState>({
+    isOpen: false,
+    src: '',
+    width: 0,
+    height: 0,
+  })
+
+  const onOpenImageModal = (src: string, width: number, height: number) => {
+    setModalState({
+      isOpen: true,
+      src: src,
+      width: width,
+      height: height,
+    })
+  }
+
+  const onCloseImageModal = () => {
+    setModalState({
+      isOpen: false,
+      src: '',
+      width: 0,
+      height: 0,
+    })
+  }
   return (
     <Wrap>
       <StyledMasonry
@@ -15,9 +56,29 @@ export const PhotoContent = ({ data }: Props) => {
             alt={item.title}
             width={item.image.width}
             height={item.image.height}
+            onClick={() =>
+              onOpenImageModal(
+                item.image.url,
+                item.image.width,
+                item.image.height
+              )
+            }
           />
         ))}
       </StyledMasonry>
+      <Modal open={modalState.isOpen} onClose={onCloseImageModal}>
+        <ImageModal>
+          <ModalImage
+            src={modalState.src}
+            width={modalState.width}
+            height={modalState.height}
+            alt=""
+          />
+          <CloseModal onClick={onCloseImageModal}>
+            <ScreenReaderText>閉じる</ScreenReaderText>
+          </CloseModal>
+        </ImageModal>
+      </Modal>
     </Wrap>
   )
 }
--- components/pages/PhotoContent/styles.ts
+++ components/pages/PhotoContent/styles.ts
@@ -12,3 +12,71 @@ export const StyledImage = styled(Image)`
   width: 100%;
   height: auto;
 `
+
+export const ImageModal = styled.div`
+  background-color: #000;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 96vw;
+  height: 96vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+  box-sizing: border-box;
+`
+
+export const ImageModalInner = styled.div`
+  padding: 24px;
+`
+
+export const ModalImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`
+
+export const CloseModal = styled.button`
+  position: fixed;
+  top: 12px;
+  right: 12px;
+  padding: 0;
+  width: 24px;
+  height: 24px;
+  border: none;
+  background-color: transparent;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &::before,
+  &::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 2px;
+    background-color: #fff;
+    position: absolute;
+    inset: 0;
+    margin: auto;
+  }
+
+  &::before {
+    transform: rotate(45deg);
+  }
+
+  &::after {
+    transform: rotate(-45deg);
+  }
+`
+
+export const ScreenReaderText = styled.span`
+  clip: rect(1px, 1px, 1px, 1px);
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+`
