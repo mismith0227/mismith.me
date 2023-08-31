7a6c2c7f2e257a25da3f2be833d5af11583b5e8d
mismith0227 <mail@mismith.me>
2023-08-31 18:48:47 +0900

    twitter 埋め込み

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,17 @@ import { Global } from '@emotion/react'
 import { globalStyle } from '@/styles/globalStyles'
 import type { AppProps } from 'next/app'
 import { usePageView } from '@/hooks/usePageView'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   usePageView()
+
+  useEffect(() => {
+    const s = document.createElement('script')
+    s.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+    s.setAttribute('async', 'true')
+    document.head.appendChild(s)
+  }, [])
 
   return (
     <>
