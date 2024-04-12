99a56f47a5e4a7513d3bbfdc7d4f3567a8a280af
mismith0227 <mail@mismith.me>
2024-04-12 09:09:39 +0900

    画像を表示

--- components/pages/SeriesContainer/index.tsx
+++ components/pages/SeriesContainer/index.tsx
@@ -1,6 +1,38 @@
-import { StyledContainer } from './styles'
+import Image from 'next/image'
+import { StyledContainer, StyledThumbnail } from './styles'
 import { Props } from './types'
 
 export const SeriesContainer = ({ data }: Props) => {
-  return <StyledContainer size="lg">シリーズ</StyledContainer>
+  const renderImages = (
+    images?: { url: string; width: number; height: number }[]
+  ) => {
+    if (!images) {
+      return null
+    }
+    const filteredImages = images.slice(0, 2)
+
+    return (
+      <div>
+        {filteredImages.map((image) => (
+          <StyledThumbnail
+            src={image.url}
+            key={image.url}
+            alt=""
+            width={image.width}
+            height={image.height}
+          />
+        ))}
+      </div>
+    )
+  }
+  return (
+    <StyledContainer size="lg">
+      {data.map((item) => (
+        <div key={item.id}>
+          <div>{item.category_name}</div>
+          {renderImages(item.images)}
+        </div>
+      ))}
+    </StyledContainer>
+  )
 }
--- components/pages/SeriesContainer/styles.ts
+++ components/pages/SeriesContainer/styles.ts
@@ -4,13 +4,7 @@ import { Container } from '@/components/organisms/Container'
 
 export const StyledContainer = styled(Container)``
 
-export const StyledMainImage = styled(Image)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 1;
+export const StyledThumbnail = styled(Image)`
   width: 100%;
   height: 100%;
-  object-fit: cover;
-  object-position: top;
 `
