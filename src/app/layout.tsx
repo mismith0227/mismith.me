db0dfd7a0a1e4ae820b65a791a0b40b8cc616814
mismith0227 <mail@mismith.me>
2025-03-05 20:16:53 +0900

    tailwindcssとapp routerの導入

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'My App',
+  description: 'This is my app description',
+  openGraph: {
+    type: 'website',
+    url: 'https://your-site.com',
+  },
+}
+
+import '@/src/styles/globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja" prefix="og: https://ogp.me/ns#">
+      <head>
+        {/* {existsGaId && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: '${pathname}',
+                  });`,
+              }}
+            />
+          </>
+        )} */}
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col justify-center items-center px-3 max-w-screen-lg mx-auto py-32">
+      <div className="text-2xl font-bold underline">title</div>
+    </div>
+  )
+}
+
+export default Home
