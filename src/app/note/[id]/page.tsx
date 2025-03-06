5a006068847a1e990f49cb92db7c6fb0430ade64
mismith0227 <mail@mismith.me>
2025-03-06 17:36:54 +0900

    note詳細画面のスタイリング

--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { formatDate } from '@/utils/formatDate'
 import { getAllNoteIds } from '../api/getAllNoteIds'
 import { getNotePost } from '../api/getNotePost'
+import Image from 'next/image'
+import { LinkButton } from '@/src/components/LinkButton'
 
 export default async function BlogPostDetailPage({
   params,
@@ -7,13 +10,36 @@ export default async function BlogPostDetailPage({
   params: Promise<{ id: string; category: string }>
 }) {
   const { id } = await params
-  const data = await getNotePost(id)
+  const { publishedAt, title, thumbnail, content } = await getNotePost(id)
 
   return (
     <main>
-      <h1>{data.title}</h1>
-      <div>カテゴリー：{data.category && data.category.category_name}</div>
-      <div dangerouslySetInnerHTML={{ __html: data.content }} />
+      <div className="text-xs">
+        <span className="inline-block mr-[2] font-bold">公開日:</span>
+        {formatDate(publishedAt)}
+      </div>
+      <h1 className="text-[24px] md:text-[32px] font-bold mt-4">{title}</h1>
+      {thumbnail && (
+        <Image
+          src={thumbnail?.url}
+          alt={title}
+          width={thumbnail.width}
+          height={thumbnail.height}
+          className="w-full h-full object-cover align-bottom mt-[24px]"
+        />
+      )}
+
+      <div
+        dangerouslySetInnerHTML={{ __html: content }}
+        className="mt-[60px] md:mt-[80px]"
+      />
+
+      <LinkButton
+        href="/note"
+        className="mt-[80px] ml-auto mr-auto w-full max-w-[300px]"
+      >
+        Note一覧へ
+      </LinkButton>
     </main>
   )
 }
---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import { twMerge } from 'tailwind-merge'
+
+type Props = {
+  className?: string
+  children: React.ReactNode
+  href: string
+  isexternal?: boolean
+}
+
+export const LinkButton = ({
+  children,
+  href,
+  isexternal,
+  className,
+}: Props) => {
+  return (
+    <Link
+      href={href}
+      target={isexternal ? '_blank' : undefined}
+      className={twMerge(
+        'relative flex items-center justify-center px-6 py-2 text-white bg-black text-center text-sm font-bold border-2 border-black transition duration-200 hover:text-black hover:bg-white box-border',
+        className
+      )}
+    >
+      {children}
+    </Link>
+  )
+}
