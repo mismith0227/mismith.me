5b95a2a7cf51686493c528e727f9e03e3a24bb57
mismith0227 <mail@mismith.me>
2025-07-02 00:28:39 +0900

    note用のapi追加

--- src/app/note/api/getAllNoteIds.ts
+++ src/app/note/api/getAllNoteIds.ts
@@ -1,8 +1,7 @@
 import { client } from '@/libs/client'
 
 export const getAllNoteIds = async (): Promise<string[]> => {
-  const contentIds = await client.getAllContentIds({ endpoint: 'blog' })
-  const filteredData = contentIds.filter((item) => item.startsWith('note-'))
+  const contentIds = await client.getAllContentIds({ endpoint: 'note' })
 
-  return filteredData
+  return contentIds
 }
